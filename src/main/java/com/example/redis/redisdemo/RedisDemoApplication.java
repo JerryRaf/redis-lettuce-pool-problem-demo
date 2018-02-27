7fb4289055c4fb996595ccda9cd0d27719328cdc
longzhe <529039038@qq.com>
2018-02-27 17:51:34 +0800

    first commit

---
+++ src/main/java/com/example/redis/redisdemo/RedisDemoApplication.java
@@ -0,0 +1,43 @@
+package com.example.redis.redisdemo;
+
+import java.nio.charset.StandardCharsets;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class RedisDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisDemoApplication.class, args);
+	}
+
+	@RestController
+	public static class RedisController {
+
+		@Autowired
+		private RedisConnectionFactory redisConnectionFactory;
+
+		@Autowired
+		private StringRedisTemplate stringRedisTemplate;
+
+
+		@GetMapping(value = "/getRedisCluster")
+		public String getRedisCluster() {
+			byte[] bytes = redisConnectionFactory.getClusterConnection().stringCommands()
+					.get("key".getBytes());
+			if (bytes != null) {
+				String value = new String(bytes, StandardCharsets.UTF_8);
+				String stringValue = stringRedisTemplate.opsForValue().get("stringKey");
+				return value + "_" + stringValue;
+			} else {
+				return "error";
+			}
+		}
+	}
+}
